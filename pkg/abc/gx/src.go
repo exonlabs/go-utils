8bbe132956a532eca5370c4f1c71bdce1411be0a
Mohamed Mohsen <mmsayed.dev@gmail.com>
2025-02-26 11:36:48 +0200

    replaced gx.Ordered with standard lib cmp.Ordered

--- pkg/abc/gx/src.go
+++ pkg/abc/gx/src.go
@@ -4,22 +4,14 @@
 
 package gx
 
-// Ordered is a constraint that allows any ordered type, i.e.,
-// types that support comparison operators like <, <=, >=, and >.
-// This includes various numeric types (integers and floats) and strings.
-type Ordered interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-		~float32 | ~float64 |
-		~string
-}
+import "cmp"
 
 // Max returns the largest value among a fixed number of arguments of type [Ordered].
 // At least one argument must be passed, and it returns that if it's the only one.
 // It compares all the provided values and returns the maximum value.
 //
 //	max := Max(3, 5, 2)   // max is 5
-func Max[T Ordered](first T, rest ...T) T {
+func Max[T cmp.Ordered](first T, rest ...T) T {
 	r := first
 	for _, v := range rest {
 		if v > r {
@@ -34,7 +26,7 @@ func Max[T Ordered](first T, rest ...T) T {
 // It compares all the provided values and returns the minimum value.
 //
 //	min := Min(3, 5, 2)   // min is 2
-func Min[T Ordered](first T, rest ...T) T {
+func Min[T cmp.Ordered](first T, rest ...T) T {
 	r := first
 	for _, v := range rest {
 		if v < r {
